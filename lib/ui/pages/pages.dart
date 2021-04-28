af07d0af3f9c2f5a5b4601bd62cfd33181c479be
IndraYudha22 <32633599+IndraYudha22@users.noreply.github.com>
2021-04-28 10:47:48 +0700

    update: setup and create general page

---
+++ lib/ui/pages/pages.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_application/shared/shared.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:supercharged/supercharged.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+part 'general_page.dart';
+part 'sign_in_page.dart';
