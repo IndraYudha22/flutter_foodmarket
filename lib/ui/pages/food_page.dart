54faec62ae698a9b9c53796fcaf912e03fea3019
IndraYudha22 <32633599+IndraYudha22@users.noreply.github.com>
2021-04-28 19:15:25 +0700

    update: add food page

---
+++ lib/ui/pages/food_page.dart
@@ -0,0 +1,59 @@
+part of 'pages.dart';
+
+class FoodPage extends StatefulWidget {
+  @override
+  _FoodPageState createState() => _FoodPageState();
+}
+
+class _FoodPageState extends State<FoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Column(
+          children: [
+            //Header
+            Container(
+              color: Colors.white,
+              height: 100,
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Food Market',
+                        style: blackFontStyle1,
+                      ),
+                      Text(
+                        "Let's get some food",
+                        style:
+                            greyFontStyle.copyWith(fontWeight: FontWeight.w300),
+                      )
+                    ],
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                'https://i.pinimg.com/474x/8a/f4/7e/8af47e18b14b741f6be2ae499d23fcbe.jpg'),
+                            fit: BoxFit.cover)),
+                  )
+                ],
+              ),
+            )
+            //List Of Food
+            //List Of Food (Tabs)
+          ],
+        )
+      ],
+    );
+  }
+}
