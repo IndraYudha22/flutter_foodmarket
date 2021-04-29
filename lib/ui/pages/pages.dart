ae47d3bdd874ad742d1000afcf1a6416a6907d0f
IndraYudha22 <32633599+IndraYudha22@users.noreply.github.com>
2021-04-29 16:39:22 +0700

    update: add food detail page

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter_application/models/models.dart';
 import 'package:flutter_application/shared/shared.dart';
 import 'package:flutter_application/ui/widgets/widgets.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:supercharged/supercharged.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -14,3 +17,4 @@ part 'sign_up_page.dart';
 part 'address_page.dart';
 part 'main_page.dart';
 part 'food_page.dart';
+part 'food_detail_page.dart';
