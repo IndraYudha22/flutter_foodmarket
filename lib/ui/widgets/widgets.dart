51c3293d9c25a72ba0c7eee7641b98026c4e657f
IndraYudha22 <32633599+IndraYudha22@users.noreply.github.com>
2021-04-29 13:05:09 +0700

    update: add food list item

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/models/models.dart';
 import 'package:flutter_application/shared/shared.dart';
+import 'package:intl/intl.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:supercharged/supercharged.dart';
 
@@ -9,3 +10,4 @@ part 'custom_bottom_navbar.dart';
 part 'food_card.dart';
 part 'rating_stars.dart';
 part 'custom_tabbar.dart';
+part 'food_list_item.dart';
