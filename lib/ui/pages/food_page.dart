9403eb9bcc570d819042fa32e5d22cac19bacd93
IndraYudha22 <32633599+IndraYudha22@users.noreply.github.com>
2021-04-29 12:40:18 +0700

    update: add custom tab bar

--- lib/ui/pages/food_page.dart
+++ lib/ui/pages/food_page.dart
@@ -6,6 +6,8 @@ class FoodPage extends StatefulWidget {
 }
 
 class _FoodPageState extends State<FoodPage> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -72,6 +74,42 @@ class _FoodPageState extends State<FoodPage> {
               ),
             ),
             //List Of Food (Tabs)
+            Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  CustomTabBar(
+                    titles: ['New Taste', 'Popular', 'Recommended'],
+                    selectedIndex: selectedIndex,
+                    onTap: (index) {
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Builder(builder: (_) {
+                    String body = (selectedIndex == 0)
+                        ? 'New Taste Body'
+                        : (selectedIndex == 1)
+                            ? 'Popular Body'
+                            : 'Recommended Body';
+                    return Center(
+                      child: Text(
+                        body,
+                        style: blackFontStyle2,
+                      ),
+                    );
+                  }),
+                  SizedBox(
+                    height: 80,
+                  )
+                ],
+              ),
+            )
           ],
         )
       ],
