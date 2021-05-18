9c9d9364208a0bb86cb2e415dfab432fc493df05
IndraYudha22 <32633599+IndraYudha22@users.noreply.github.com>
2021-05-18 13:35:52 +0700

    update : add order history page

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -34,12 +34,7 @@ class _MainPageState extends State<MainPage> {
                 child: FoodPage(),
               ),
               Center(
-                child: IllustrationPage(
-                    title: 'Ouch! hungry',
-                    subtitle: 'Seems you like have not\nordered any food yet',
-                    picturePath: 'assets/love_burger.png',
-                    buttonTap1: () {},
-                    buttonTitle1: 'Find Foods'),
+                child: OrderHistoryPage()
               ),
               Center(
                 child: Text('Profile'),
